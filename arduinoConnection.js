3fe2a5b7158112196682e5cece4d0986cdf92cff
Isaac <intosite@gmail.com>
2016-01-28 16:38:39 +0800

    Update: Added HC-SR04 sonar sensor

--- arduinoConnection.js
+++ arduinoConnection.js
@@ -7,25 +7,40 @@ var ArduinoConnection = function() {
     console.log('[ArduinoConnection]');
     var self = this;
 	var board = new five.Board(); 
-	var pinLeft, pinRight;
+	var pinLeft, pinRight, sonar;
 	var customEvent = new EventEmitter();
+	var reading = 0;
 
 	board.on("ready", function() {
 		console.log( "[ArduinoConnection] - board ready" );
-		pinLeft = new five.Button({
-			pin: 8,
-			isPullup: true
+		sonar = new five.Proximity({
+			controller: "HCSR04",
+			pin: 7,
+			freq: 500
 		});
-		pinLeft.on("down", function(value) {
-		    self.emit("data", "left");
+		sonar.on("change", function() {
+			if (reading != Math.round(this.cm) ) {
+				reading = Math.round(this.cm);
+				// console.log("  cm  : ", reading);
+				// console.log("-----------------");
+				self.emit("data", reading);
+			}
+			
 		});
-		pinRight = new five.Button({
-			pin: 6,
-			isPullup: true
-		});
-		pinRight.on("down", function(value) {
-		    self.emit("data", 'right');
-		});
+		// pinLeft = new five.Button({
+		// 	pin: 8,
+		// 	isPullup: true
+		// });
+		// pinLeft.on("down", function(value) {
+		//     self.emit("data", "left");
+		// });
+		// pinRight = new five.Button({
+		// 	pin: 6,
+		// 	isPullup: true
+		// });
+		// pinRight.on("down", function(value) {
+		//     self.emit("data", 'right');
+		// });
 	});
 };
 
